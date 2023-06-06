fbf8eaeebf93b96b670158ac134cf0aa93c10377
Eminent-michael <michaelafolab@gmail.com>
2023-06-06 17:40:36 +0100

    Reaction-timer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer App</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if='showResult'>Reaction Time: {{score}}ms</p>
+  <Results :result="score" v-if="showResult"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block, Results},
+  data() {
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -22,5 +45,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button{
+  background-color: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+  border-radius: 4px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>{{ clickRate }}</h2>
+</template>
+
+<script>
+    export default{
+        props: ["result", "show"],
+
+        data() {
+            return{
+                clickRate: null,
+            }
+        },
+        mounted() {
+            if (this.result < 250) {
+                this.clickRate = "Flash Finger"
+            }else if(this.result < 400) {
+                this.clickRate = "Rapid Reflexes"
+            }else{
+            console.log(this.result)
+                this.clickRate = "Snail Pace...."
+            }
+        },
+    }
+</script>
+
+<style>
+    h2 {
+        color: #0faf87;
+    }
+</style>
